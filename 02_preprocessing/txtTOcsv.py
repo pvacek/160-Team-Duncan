c9fe5b3096dd4fa7f81ad579835aaa2c10d952aa
DESKTOP-GIKQ04K\ckchiruka <ckchiruka@gmail.com>
2017-05-17 11:47:03 -0700

    Final data cleaning script and example

--- 02_preprocessing/txtTOcsv.py
+++ 02_preprocessing/txtTOcsv.py
@@ -9,7 +9,7 @@ import sys
 
 
 def parseValues(snapshot):
-    lines=snapshot.replace(']', '').replace('[', '').strip('\n').split(',')
+    lines = snapshot.replace(']', '').replace('[', '').strip('\n').split(',')
     return lines
     
 
@@ -17,8 +17,16 @@ def main(filename = sys.argv[1]):
     values = open(filename).readlines()
     values = [parseValues(v) for v in values]
     values = pd.DataFrame(values).drop_duplicates()
-    filename = filename.replace('.txt', '') + '.csv'
-    values.to_csv(filename)
+    values = values.drop([4,5,6,10,14,15,19,20,24,25,29,30,34,35,39,40,44,45,49,50,54,55,59], axis = 1)
+    
+    colNames = ['quarter', 'unique_id', 'sec_in_quarter', 'shot_clock', 'ball_x', 'ball_y', 'ball_z']
+    for i in range(1,11):
+        colNames.append('player'+str(i)+'_id')
+        colNames.append('player'+str(i)+'_x')
+        colNames.append('player'+str(i)+'_y')
+    
+    filename = filename.replace('.txt', '.csv')
+    values.to_csv(filename, index = False, header = colNames)
 
     
 if __name__ == "__main__" : main()
